39e01272712bb58230e0d3b373e1842f0cd9dcd7
BigSteveLittle <bigsteve@redandblackzone.com>
2023-02-10 19:48:23 +1100

    Makes HTML Formatting Changes

--- resources/views/crust.blade.php
+++ resources/views/crust.blade.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html lang="en-AU">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Crust</title>
-</head>
-
-<body>
-    <h1>{{ $heading1 }}</h1>
-        <h2>{{ $crust['crustTitle'] }}</h2>
-            <p>{{ $crust['crustDescription'] }}</p>
-</body>
-
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Crust</title>
+    </head>
+    <body>
+        <h1>{{ $heading1 }}</h1>
+            <h2>{{ $crust['crustTitle'] }}</h2>
+                <p>{{ $crust['crustDescription'] }}</p>
+    </body>
 </html>
